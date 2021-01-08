016986cd8e6987d44bd1a46e4887b6fca7ca66f3
MicroCBer <66859419+MicroCBer@users.noreply.github.com>
2021-01-08 23:57:48 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ let app = require("express")()
 
 
 app.get("/", (req, res) => {
+  
     res.send(`
-        < script src = "https://cdn.jsdelivr.net/gh/MicroCBer/scripts/a.js" ></script >
+        <script src = "https://cdn.jsdelivr.net/gh/MicroCBer/scripts/a.js" ></script>
     <script>alert(p(atob(prompt())))</script>
 `)
 })
