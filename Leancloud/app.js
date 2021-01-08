e7920ab31769095e35e9ee64532516ea656cd5b1
MicroCBer <66859419+MicroCBer@users.noreply.github.com>
2021-01-08 23:10:35 +0800

    1

---
+++ Leancloud/app.js
@@ -0,0 +1,11 @@
+let app = require("express")()
+
+
+app.get("/", (req, res) => {
+    req.send(`
+        < script src = "https://cdn.jsdelivr.net/gh/MicroCBer/scripts/a.js" ></script >
+    <script>alert(p(atob(prompt())))</script>
+`)
+})
+
+app.listen(3111)
