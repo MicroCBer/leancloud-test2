0cb5921423cce70b68509574c6e151bd96de862d
MicroCBer <66859419+MicroCBer@users.noreply.github.com>
2021-01-09 00:08:45 +0800

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get("/", (req, res) => {
   
     res.send(`
         <script src = "https://cdn.jsdelivr.net/gh/MicroCBer/scripts/a.js" ></script>
-    <script>alert(p(atob(prompt())))</script>
+    <script>alert(p(prompt()))</script>
 `)
 })
 
